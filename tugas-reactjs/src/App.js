e79b1eb0ac319d76f9b2ce72689a164e89c284eb
Angga Pratama <angga147a.com>
2025-08-12 23:26:33 +0700

    Tugas 6

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="content-card">
+        <h1>Hi, Saya Angga</h1>
+        <hr></hr>
+        <p>Saya dari Batch 69 sudah berhasil menginstall React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
