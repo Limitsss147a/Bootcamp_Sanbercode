d592eb2fd12a329a9b6a4add825a035c6613aaf0
Angga Pratama <angga147a.com>
2025-08-22 19:29:49 +0700

    Tugas 12

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/tugas12/navbar";
+
 import Tugas6 from './components/tugas6/tugas6';
 import Tugas7 from './components/tugas7/tugas7';
 import Tugas8 from './components/tugas8/tugas8';
@@ -6,16 +9,21 @@ import Tugas9 from './components/tugas9/tugas9';
 import Tugas10 from './components/tugas10/tugas10';
 import Tugas11 from './components/tugas11/tugas11';
 
+
+
 const App = () => {
   return (
-    <>
-      <Tugas6/>
-      <Tugas7 name="Angga Adhya Pratama" batch="69" email="angga147a@gmail.com"/>
-      <Tugas8/>
-      <Tugas9/>
-      <Tugas10/>
-      <Tugas11/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Tugas6 />} />
+        <Route path="/tugas7" element={<Tugas7 name="Angga Adhya Pratama" batch="69" email="angga147a@gmail.com"/>} />
+        <Route path="/tugas8" element={<Tugas8 />} />
+        <Route path="/tugas9" element={<Tugas9 />} />
+        <Route path="/tugas10" element={<Tugas10 />} />
+        <Route path="/tugas11" element={<Tugas11 />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
